ba867915addc8a84a0d41d7d11ac7815d54279f9
thisishwan2 <feel000617@gmail.com>
2024-05-14 01:52:02 +0900

    Initial commit

---
+++ src/main/java/org/example/tomcat_nio_practice/SpringController.java
@@ -0,0 +1,2 @@
+package org.example.tomcat_nio_practice;public class SpringController {
+}
---
+++ src/test/java/org/example/tomcat_nio_practice/TomcatNioPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tomcat_nio_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatNioPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
