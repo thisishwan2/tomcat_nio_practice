ea71fa35f39615f2ce8fcc25b1fba311a640f016
thisishwan2 <feel000617@gmail.com>
2024-05-14 01:52:16 +0900

    initial setting

--- src/main/java/org/example/tomcat_nio_practice/SpringController.java
+++ src/main/java/org/example/tomcat_nio_practice/SpringController.java
@@ -1,2 +1,46 @@
-package org.example.tomcat_nio_practice;public class SpringController {
+package org.example.tomcat_nio_practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.sql.Time;
+import java.util.logging.Logger;
+
+@RestController
+public class SpringController {
+
+    Logger logger = Logger.getLogger(SpringController.class.getName());
+
+
+    // 동기 처리 방식
+//    @GetMapping("/spring")
+//    public String spring() throws InterruptedException {
+//        RestTemplate restTemplate = new RestTemplate();
+//        logger.info("start spring");
+//        System.out.println(Thread.currentThread().getName());
+//        String result = restTemplate.getForObject("http://localhost:5000/ai", String.class);
+//        logger.info("end spring");
+//        return "success";
+//    }
+
+
+
+
+    @GetMapping("/spring")
+    public String spring() {
+        logger.info("start spring");
+        WebClient webClient = WebClient.create("http://localhost:5000");
+
+        webClient.get()
+                .uri("/ai")
+                .retrieve()
+                .bodyToMono(String.class)
+                .subscribe(result -> {
+                    logger.info("결과: " + result);
+                });
+
+        logger.info("end spring");
+        return "a";
+    }
 }
--- src/main/java/org/example/tomcat_nio_practice/TomcatNioPracticeApplication.java
+++ src/main/java/org/example/tomcat_nio_practice/TomcatNioPracticeApplication.java
@@ -2,7 +2,9 @@ package org.example.tomcat_nio_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class TomcatNioPracticeApplication {
 
